3e2f1f9072b2513c24192714a8c89fd9cea69c03
Justin <truonghoangminhduc123@gmail.com>
2026-08-31 10:21:53 +0700

    Update bot host configuration

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(PORT, () => {
 
 // --- 2. CẤU HÌNH BOT "dot" ---
 const BOT_CONFIG = {
-  host: 'minhducz.seedloaf.gg', // IP Server của Đức
+  host: 'minhducz.gimzonodes.space', // IP Server của Đức
   port: 25565,                   // Port server
   username: 'dot',               // Tên bot
   version: '1.21.11',             // FIX: Bản protocol chuẩn để tránh lỗi ProtocolMismatch/Netty socket (Server 1.20+ / ViaVersion đều nhận)
